0ab42a9fdb64add81cc44e2592a8c0eeaf0cd88c
Niklas Stemberg <niklas.stemberg@app-akademie.com>
2023-09-26 10:50:15 +0200

    API Integration

---
+++ lib/features/game_configuration/domain/category_model.dart
@@ -0,0 +1,9 @@
+class Category {
+  String id;
+  String name;
+
+  Category({required this.id, required this.name});
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      Category(id: json["id"], name: json["name"]);
+}
--- lib/features/game_configuration/presentation/pages/category_selection_page.dart
+++ lib/features/game_configuration/presentation/pages/category_selection_page.dart
@@ -1,13 +1,14 @@
-import 'dart:io';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+
 import 'package:quiz_app/core/presentation/styles/color_styles.dart';
 import 'package:quiz_app/core/presentation/styles/position_styles.dart';
+import 'package:http/http.dart' as http;
 import 'package:quiz_app/core/presentation/styles/text_styles.dart';
 import 'package:quiz_app/core/presentation/widgets/custom_app_bar.dart';
 import 'package:quiz_app/core/presentation/widgets/primary_button.dart';
-import 'package:quiz_app/features/onboarding/domain/user_model.dart';
+import 'package:quiz_app/features/game_configuration/domain/category_model.dart';
 
 class CategorySelectionPage extends StatefulWidget {
   const CategorySelectionPage({super.key});
@@ -17,7 +18,22 @@ class CategorySelectionPage extends StatefulWidget {
 }
 
 class _CategorySelectionPageState extends State<CategorySelectionPage> {
-  Box<User> userBox = Hive.box<User>("userBox");
+  Future<List<Category>> fetchCategories() async {
+    Uri url =
+        Uri.https("run.mocky.io", "v3/6f0cc8e3-1e9f-4c00-8452-af1eda920146");
+    debugPrint("Anfrage wird gesendet...");
+    http.Response response = await http.get(url);
+    debugPrint(response.body);
+    var jsonResponse = jsonDecode(response.body);
+
+    if (jsonResponse.runtimeType == List) {
+      return (jsonResponse as List)
+          .map((val) => Category.fromJson(val))
+          .toList();
+    } else {
+      return [Category.fromJson(jsonResponse)];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +47,40 @@ class _CategorySelectionPageState extends State<CategorySelectionPage> {
             const CustomAppBar(
               text: "Kategorien",
             ),
-            Image.file(File(userBox.get("1")?.avatarImagePath ?? "")),
-            Text(
-              userBox.get("1")?.username ?? "Kein Nutzer gefunden",
-              style: kTextHeadline2,
-            ),
+            FutureBuilder(
+                future: fetchCategories(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<Category> categoryList = snapshot.data ?? [];
+                    debugPrint(categoryList.toString());
+                    return Expanded(
+                      child: ListView(
+                        children: categoryList
+                            .map(
+                              (val) => Padding(
+                                padding: kPaddingVerLarge,
+                                child: Container(
+                                  height: 60,
+                                  width: MediaQuery.of(context).size.width - 40,
+                                  color: kColorSecondary,
+                                  child: Center(
+                                    child: Text(
+                                      val.name,
+                                      style: kTextHeadline2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    );
+                  }
+                  return const Text(
+                    "Lädt",
+                    style: kTextBody,
+                  );
+                }),
             PrimaryButton(
               text: "Weiter",
               onPressed: () {
